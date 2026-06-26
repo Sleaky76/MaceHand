649e2c652ab7796f96860dfa1374cbb066c85178
Loris <sleakyalt@gmail.com>
2026-06-26 19:26:03 +0200

    Add existing project files to Git

---
+++ src/main/java/com/sleaky/maceHands/MaceHands.java
@@ -0,0 +1,17 @@
+package com.sleaky.maceHands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MaceHands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
